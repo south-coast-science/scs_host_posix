12923dba59fc99b98bc8f38faf9d39c37d6aa8bf
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-01-19 11:38:27 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # scs_host_posix
 Host abstractions for data consumers running POSIX-compatible operating systems.
+
+Required libraries: 
+
+* Third party: pycurl
+* SCS root: scs_core
---
+++ scs_host/sys/host.py
@@ -0,0 +1,15 @@
+"""
+Created on 20 Nov 2016
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class Host(object):
+    """
+    Any Darwin Mac
+    """
+
+    SCS_OSIO = '/Users/bruno/Documents/Development/SCS/osio/'      # hard-coded path
