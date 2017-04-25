b5b4b7693295a1f252f34957188a3c36523d61d7
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-04-25 11:18:44 +0100

    Added osio_topic_subscriber top-level script.

--- scs_host/sys/host.py
+++ scs_host/sys/host.py
@@ -14,10 +14,15 @@ class Host(object):
     Any Darwin Mac
     """
 
-    __SCS_OSIO = 'SCS/osio/'      # hard-coded path
+    __SCS_CONF =    'SCS/conf/'         # hard-coded path
+    __SCS_OSIO =    'SCS/osio/'         # hard-coded path
 
 
     # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def conf_dir(cls):
+        return os.path.expanduser('~') + '/' + cls.__SCS_CONF
 
     @classmethod
     def osio_dir(cls):
