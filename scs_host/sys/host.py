bfe39e9b1211ea7626aedc61f30d6324c5eac835
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-05-09 10:43:11 +0100

    Added attn field to ControlDatum.

--- scs_host/sys/host.py
+++ scs_host/sys/host.py
@@ -2,9 +2,12 @@
 Created on 20 Nov 2016
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+http://stackoverflow.com/questions/4271740/how-can-i-use-python-to-get-the-system-hostname
 """
 
 import os
+import socket
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -21,8 +24,14 @@ class Host(object):
     # ----------------------------------------------------------------------------------------------------------------
 
     @classmethod
+    def name(cls):
+        return socket.gethostname()
+
+
+    @classmethod
     def conf_dir(cls):
         return os.path.expanduser('~') + '/' + cls.__SCS_CONF
+
 
     @classmethod
     def osio_dir(cls):
