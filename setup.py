90ed43e787393d146e1765063ab1016bca139f8d
Gemma Hentsch <gemma@halfapenguin.com>
2017-11-19 21:14:30 +0000

    With manifest version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='scs_host_posix',
-    version='0.1.2',
+    version='0.1.3',
     description='Abstractions for POSIX based systems',
     author='South Coast Science',
     author_email='contact@southcoastscience.com',
