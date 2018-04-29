4042b3e53ea9255be73974a8b6979d4ff338ea7a
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-04-29 13:35:59 +0100

    Added host_disk_usage_test.

--- tests/sys/host_disk_usage_test.py
+++ tests/sys/host_disk_usage_test.py
@@ -1,37 +1,19 @@
 #!/usr/bin/env python3
 
 """
-Created on 16 Apr 2018
+Created on 29 Apr 2018
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 """
 
-from scs_core.data.json import JSONify
-
 from scs_host.sys.host import Host
 
 
-# TODO: add disk usage command to scs_mfr
-
 # --------------------------------------------------------------------------------------------------------------------
 
-volume = '/Users/bruno'
-usage = Host.disk_usage(volume)
-print("volume: %s usage: %s" % (volume, usage))
+home_dir = Host.home_dir()
+print("home_dir: %s" % home_dir)
 
-print(JSONify.dumps(usage.as_json()))
-print("-")
+du = Host.disk_usage(home_dir)
+print("du: %s" % du)
 
-volume = '/'
-usage = Host.disk_usage(volume)
-print("volume: %s usage: %s" % (volume, usage))
-
-print(JSONify.dumps(usage.as_json()))
-print("-")
-
-
-volume = '/Volumes/non-existent'
-usage = Host.disk_usage(volume)
-print("volume: %s usage: %s" % (volume, usage))
-
-print(JSONify.dumps(usage.as_json()))
