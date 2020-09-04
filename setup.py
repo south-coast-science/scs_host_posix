c7fb2e0885e80f4fe061c40ce41584fe0035fb64
jade <jadempage@outlook.com>
2020-09-04 10:55:18 +0100

    Updated setup.py files to work with PyPi

--- setup.py
+++ setup.py
@@ -1,42 +1,31 @@
-from setuptools import setup, find_packages
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 with open('requirements.txt') as req_txt:
     required = [line for line in req_txt.read().splitlines() if line]
 
-try:
-    # noinspection PyUnresolvedReferences
-    import pypandoc
-    # noinspection PyUnresolvedReferences
-    from os import path
-    here = path.abspath(path.dirname(__file__))
-    long_description = pypandoc.convert(path.join(here, 'README.md'), 'rst'),
-except RuntimeError:
-    long_description = ""
-except ImportError:
-    long_description = ""
-
-setup(
-    name='scs_host_posix',
-    version='0.1.4',
-    description='Abstractions for POSIX based systems',
-    author='South Coast Science',
-    author_email='contact@southcoastscience.com',
-    url='https://github.com/south-coast-science/scs_host_posix',
+setuptools.setup(
+    name="SCS_HOST_POSIX",
+    version="0.0.2",
+    author="South Coast Science",
+    author_email="contact@southcoastscience.com",
+    description="Host abstractions for data consumers running POSIX-compliant operating systems, including Windows 10.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/south-coast-science/scs_host_posix",
     package_dir={'': 'src'},
-    packages=find_packages('src'),
+    packages=setuptools.find_packages('src'),
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX",
     ],
+    python_requires='>3.5',
     install_requires=required,
     platforms=['any'],
-    python_requires=">=3.3",
 )
